7fd864f400d0afbd02c93f4e6f9ea43d52bb0d15
Yago Lima <yagosvl@hotmail.com>
2025-11-23 21:35:55 -0300

    refactor: enhance logger

--- src/logger_setup.py
+++ src/logger_setup.py
@@ -1,3 +1,13 @@
 import logging
+import sys
 
-logger = logging.Logger("integrate_contacts_app")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("integrate_contacts_app")
